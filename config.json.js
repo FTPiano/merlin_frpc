d2ffb60909a799324a1c0f2fb1c917140aae2527
Victor Xie <FTPiano@users.noreply.github.com>
2018-05-27 21:51:45 +0800

    Repack 2.1.14

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "内网穿透利器，谁用谁知道。", 
     "home_url": "Module_frpc.asp", 
-    "md5": "7ec76bf9112b6d1d45f97d42066b6b2b", 
+    "md5": "b7f06a8b1326b6035f9384c77767d126", 
     "module": "frpc", 
     "title": "frpc穿透", 
     "version": "2.1.14"
